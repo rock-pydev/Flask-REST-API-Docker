4f9140fa2ce0257d4ddc84b5df4066ba3b382eee
west lepor <westlepor984@gmail.com>
2020-12-14 02:37:06 +0700

    updated apis

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify
 import mysql.connector
 from json_encoder import json
 
@@ -8,19 +8,33 @@ app = Flask(__name__)
 def getMysqlConnection():
     return mysql.connector.connect(user='root', host='db', port='3306', password='root', database='city')
 
+
 def query_result(query):
     db = getMysqlConnection()
     cur = db.cursor()
     cur.execute(query)
-    data = cur.fetchall()
-    return data
+    return cur
 
 
 @app.route('/getCity', methods=['GET'])
 def get_city():
     query = "SELECT * from cities"
-    data = query_result(query)
-    return json.dumps(data)
+    cur = query_result(query)
+    data = cur.fetchall()
+    result = []
+    for item in data:
+        result.append({
+            'Sell': item[0],
+            'List': item[1],
+            'Living': item[2],
+            'Rooms': item[3],
+            'Beds': item[4],
+            'Baths': item[5],
+            'Age': item[6],
+            'Acres': item[7],
+            'Taxes': item[8]
+        })
+    return json.dumps(result)
 
 
 @app.route('/addCity', methods=['POST'])
@@ -28,8 +42,12 @@ def add_city():
     query = "INSERT INTO cities \
              (Sell, List, Living, Rooms, Beds, Baths, Age, Acres, Taxes) \
              VALUES (142, 160, 28, 10, 5, 3, 60, 0.28, 3167);"
-    data = query_result(query)
-    return json.dumps(data)
+    try:
+        query_result(query)
+        return jsonify({"added": True})
+    except Exception as e:
+        print(e)
+        return jsonify({"added": False})
 
 
 @app.route('/modifyCity', methods=['PUT'])
@@ -37,15 +55,23 @@ def modify_city():
     query = "UPDATE cities \
              SET Sell=150 \
              WHERE List = 160;"
-    data = query_result(query)
-    return json.dumps(data)
+    try:
+        query_result(query)
+        return jsonify({"updated": True})
+    except Exception as e:
+        print(e)
+        return jsonify({"updated": False})
 
 
 @app.route('/deleteCity', methods=['DELETE'])
 def delete_city():
     query = "DELETE FROM Customers WHERE Sell=150;"
-    data = query_result(query)
-    return json.dumps(data)
+    try:
+        query_result(query)
+        return jsonify({"deleted": True})
+    except Exception as e:
+        print(e)
+        return jsonify({"deleted": False})
 
 
 if __name__ == '__main__':
